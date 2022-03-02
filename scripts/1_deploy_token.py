2224912c5c5c31cc539db25782e49cdf84514842
Márton Borsos <borsos.marton95@gmail.com>
2022-03-02 19:03:02 +0100

    Deploy on your own task

--- scripts/1_deploy_token.py
+++ scripts/1_deploy_token.py
@@ -1,14 +1,19 @@
-from brownie import accounts, config, TokenERC20, EasyToken
+from web3 import Web3
 from scripts.helpful_scripts import get_account
+from brownie import OurToken, network, config
 
+initial_supply = Web3.toWei(21_000_000, "ether")
 
-initial_supply = 1000000000000000000000  # 1000
-token_name = "Token"
-token_symbol = "TKN"
-
+def deploy_token():
+    account = get_account()
+    token = OurToken.deploy(
+        initial_supply,
+        {"from": account},
+        publish_source=config["networks"][network.show_active()].get("verify", False),
+    )
+    print("Deployed token!")
+    return token
 
 def main():
-    account = get_account()
-    erc20 = TokenERC20.deploy(
-        initial_supply, token_name, token_symbol, {"from": account}
-    )
+    deploy_token()
+
--- scripts/helpful_scripts.py
+++ scripts/helpful_scripts.py
@@ -1,12 +1,19 @@
-from brownie import network, accounts, config
 
-LOCAL_BLOCKCHAIN_ENVIRONMENTS = ["hardhat", "development", "mainnet-fork"]
+from brownie import accounts, network, config
 
 
-def get_account():
-    if network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+FORKED_LOCAL_ENVIRONMENTS = ["mainnet-fork", "mainnet-fork-dev"]
+LOCAL_BLOCKCHAIN_ENVIRONMENTS = ["development", "ganache-local"]
+
+
+def get_account(index=None, id=None):
+    if index:
+        return accounts[index]
+    if id:
+        return accounts.load(id)
+    if (
+        network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS
+        or network.show_active() in FORKED_LOCAL_ENVIRONMENTS
+    ):
         return accounts[0]
-    if network.show_active() in config["networks"]:
-        account = accounts.add(config["wallets"]["from_key"])
-        return account
-    return None
+    return accounts.add(config["wallets"]["from_key"])
